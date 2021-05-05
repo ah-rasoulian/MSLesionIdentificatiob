2e660428d4a1aec62ff9b7c01675695b726b4a0d
Amirhossein Rasoulian <ah.rasoulian@gmail.com>
2021-05-05 12:52:33 +0430

    creating functions to add database a list of patients, containing two examination samples which each sample contains multiple slices.

--- brainmri.py
+++ brainmri.py
@@ -1,8 +1,30 @@
+class Patient:
+    def __init__(self, first_examination, second_examination):
+        self.first_examination = first_examination
+        self.second_examination = second_examination
+
+
 class BrainMRI:
     def __init__(self):
-        pass
+        self.slices = []
+
+    def add_new_slice(self, new_slice):
+        self.slices.append(new_slice)
+
+    def get_slices(self):
+        return self.slices
 
 
 class MRISlice:
-    def __init__(self):
-        pass
+    def __init__(self, slice_image):
+        self.slice_image = slice_image
+        self.lesions = []
+
+    def add_new_lesion(self, new_lesion):
+        self.lesions.append(new_lesion)
+
+    def get_slice_image(self):
+        return self.slice_image
+
+    def get_lesions(self):
+        return self.lesions
--- database.py
+++ database.py
@@ -1,10 +1,20 @@
+from brainmri import Patient
+
+
 class Database:
     def __init__(self, parent_dir):
         self.parent_dir = parent_dir
-        self.brain_samples = []
+        self.samples = []
 
-    def read_dataset(self, parent_dir):
+    def read_dataset(self):
         pass
 
-    def add_new_sample(self):
-        pass
+    def add_new_sample(self, sample_directory: str):
+        examination_directories = [sample_directory + "/1/", sample_directory + "/2/"]
+        brain_samples = []
+        for directory in examination_directories:
+            pass
+
+        new_sample = Patient(brain_samples[0], brain_samples[1])
+        self.samples.append(new_sample)
+
