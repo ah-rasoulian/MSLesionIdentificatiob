cc20d93908c1b6c61324bd81e3a3e910d9bab1aa
Amirhossein Rasoulian <ah.rasoulian@gmail.com>
2021-05-05 14:56:16 +0430

    completing add new sample function which gets a patient directory and adds corresponding Patient object into database.

--- database.py
+++ database.py
@@ -1,4 +1,10 @@
+from brainmri import BrainMRI
+from brainmri import MRISlice
 from brainmri import Patient
+import scipy.io
+from os import listdir
+import cv2
+import numpy
 
 
 class Database:
@@ -13,8 +19,38 @@ class Database:
         examination_directories = [sample_directory + "/1/", sample_directory + "/2/"]
         brain_samples = []
         for directory in examination_directories:
-            pass
+            brain_examination = BrainMRI()
+
+            # classifying all file names based on their extension, dividing them into slices and lesions files
+            slices_files = []
+            lesions_files = []
+            for file_name in listdir(directory):
+                if file_name.endswith(".TIF") or file_name.endswith(".bmp"):
+                    slices_files.append(file_name)
+                elif file_name.endswith(".plq"):
+                    lesions_files.append(file_name)
+
+            # creating new MRI slice and adding it into brain MRI
+            for slice_file_name in slices_files:
+                slice_file_path = directory + slice_file_name
+                slice_image = cv2.imread(slice_file_path, cv2.IMREAD_GRAYSCALE)
+
+                mri_slice = MRISlice(slice_image)
+                slice_file_name_without_extension = slice_file_name.replace('.TIF', '')
+                for lesion_file_name in lesions_files:
+                    if lesion_file_name.startswith(slice_file_name_without_extension):
+                        lesion_file_path = directory + lesion_file_name
+                        lesion_file = scipy.io.loadmat(lesion_file_path)
+                        lesion_points = []
+                        x, y = lesion_file.get("xi"), lesion_file.get('yi')
+                        for i in range(len(x)):
+                            lesion_points.append([x[i][0], y[i][0]])
+                        lesion_contours = numpy.array(lesion_points).reshape((-1, 1, 2)).astype(numpy.int32)
+                        mri_slice.add_new_lesion(lesion_contours)
+
+                brain_examination.add_new_slice(mri_slice)
+
+            brain_samples.append(brain_examination)
 
         new_sample = Patient(brain_samples[0], brain_samples[1])
         self.samples.append(new_sample)
-
