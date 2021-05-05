879d9bddd612f59d1dfbdadaecd3f787745765f0
Amirhossein Rasoulian <ah.rasoulian@gmail.com>
2021-05-05 12:29:07 +0430

    Creating basic classes.

---
+++ brainmri.py
@@ -0,0 +1,8 @@
+class BrainMRI:
+    def __init__(self):
+        pass
+
+
+class MRISlice:
+    def __init__(self):
+        pass
---
+++ database.py
@@ -0,0 +1,10 @@
+class Database:
+    def __init__(self, parent_dir):
+        self.parent_dir = parent_dir
+        self.brain_samples = []
+
+    def read_dataset(self, parent_dir):
+        pass
+
+    def add_new_sample(self):
+        pass
